5b52b45bf660606d01135314c1d0b87ad022a450
Simona Lorenc <lorenc.simona@gmail.com>
2023-07-18 12:49:38 +0200

    addSetMapFunction

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-let x = 51.505
-let y = -0.09
+let x
+let y
 
-let map = L.map('map').setView([x, y], 13);
+let map = L.map('map').setView([0, 0], 13);
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
@@ -11,6 +11,7 @@ const API_URL_ALL = 'https://geo.ipify.org/api/v2/country?apiKey='
 
 const submitBtn = document.querySelector('.input__submit')
 const textInput = document.querySelector('.input__text')
+const form = document.querySelector('.input')
 
 const ipElement = document.querySelector('.location-info__ip-address')
 const locationElement = document.querySelector('.location-info__location')
@@ -19,39 +20,16 @@ const ispElement = document.querySelector('.location-info__isp')
 
 const API = 'https://geo.ipify.org/api/v2'
 const key = 'at_ieLuCAOtfnVf2paN9Bu5C1R01hosF'
-const inputText = textInput.value
 
-// function renderLocation() {
-//     const API_COUNTRY = API + '/country?' 
-//     // const numbers = /^[0-9.,!?]+$/
-//     // const letters = /^[a-zA-Z.,!?]+$/
-//     // if (letters.test(inputText)) {
-//     //     const URL = API_COUNTRY + 'apiKey=' + key + '&domain=' + inputText
-//     // } else if (numbers.test(inputText)) {
-//     //     const URL = API_COUNTRY + 'apiKey=' + key + '&ipAddres=' + inputText
-//     // }
-//     console.log(URL)
-//     fetch (API_COUNTRY + new URLSearchParams({
-//         apiKey: key,
-//         domain: inputText,
-//         ipAddress: inputText, 
-//     })) 
-//         .then(res => res.json())
-//         .then(json => {
-//             console.log(json)
-//             ipElement.innerHTML = json.ip
-//             locationElement.innerHTML = json.location.country + ', ' + json.location.region
-//             timezoneElement.innerHTML = json.location.timezone
-//             ispElement.innerHTML = json.isp
-//         })
-// }
+
 
 function renderInfo() {
-    const API_MAP = API + '/country,city,vpn?'
+    const API_MAP = API + '/country,city?'
+    const newInputText = textInput.value
     fetch (API_MAP + new URLSearchParams({
         apiKey: key,
-        domain: inputText,
-        ipAddress: inputText, 
+        domain: newInputText,
+        ipAddress: newInputText, 
     }))
         .then(res => res.json())
         .then(json => {
@@ -67,7 +45,26 @@ function renderInfo() {
         })
 }
 
-submitBtn.addEventListener('click', renderInfo)
+function getCurrentIP() {
+    fetch('https://api.ipify.org?format=json')
+      .then(response => response.json())
+      .then(data => {
+        const currentIP = data.ip;
+        textInput.value = currentIP;
+        renderInfo();
+      })
+      .catch(error => {
+        console.error('Błąd:', error);
+      });
+  }
+
+getCurrentIP()
+
+submitBtn.addEventListener('click', () => {
+    form.addEventListener('submit', function(event) {
+        event.preventDefault()})
+    renderInfo()
+})
 
 
 
